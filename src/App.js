6ffcf5de41f9a470bcf25dea74aa4e06c9511ce2
haseeb-official <haseebpp.official@gmail.com>
2022-08-27 18:16:26 +0530

    Add Base HTML structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from 'react'
 
 function App() {
-  return <div>TODO-LIST</div>
+  return (
+    <div>
+      <h1>TODO-LIST</h1>
+      <input type='text' />
+      <button>add</button>
+      <p>3 pending works remaining...</p>
+      <ul>
+        <li>
+          <input type='checkbox' />
+          <span>this is my first task</span>
+          <button>delete</button>
+        </li>
+        <li>
+          <input type='checkbox' />
+          <span>this is my second task</span>
+          <button>delete</button>
+        </li>
+        <li>
+          <input type='checkbox' />
+          <span>this is my third task</span>
+          <button>delete</button>
+        </li>
+      </ul>
+      <button>clear</button>
+    </div>
+  )
 }
 
 export default App
